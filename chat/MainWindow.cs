e0d1c8b27eac5fe81a0b9b727566bf88b1cbdd3f
ZanyGnu <ajaymartin@gmail.com>
2015-05-24 21:04:43 -0700

    basic winform  app using cef to open a chromium window in windows app

---
+++ chat/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var browser = new ChromiumWebBrowser("https://gmail.com/")
+            {
+                Dock = DockStyle.Fill
+            };
+
+            browserPanel.Controls.Add(browser);
+
+            
+        }
+    }
+}
