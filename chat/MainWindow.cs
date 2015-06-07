58353d475151d15a6f2146b585701f92bf6eb3b8
ZanyGnu <ajaymartin@gmail.com>
2015-06-06 19:43:39 -0700

    execute script into browser window and get output

--- chat/MainWindow.cs
+++ chat/MainWindow.cs
@@ -5,6 +5,8 @@ namespace chat
     using CefSharp.WinForms;
     using System;
     using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows.Forms;
 
     public partial class MainWindow : Form
@@ -19,15 +21,50 @@ namespace chat
             };
 
             Cef.Initialize(cefsettings);
-            
+
             var browser = new ChromiumWebBrowser("https://web.whatsapp.com/")
             {
                 Dock = DockStyle.Fill,
             };
 
             browserPanel.Controls.Add(browser);
-            
-            
+
+            Run(delegate
+            {
+                // Get Document Height
+                var task = browser.EvaluateScriptAsync("(function() { var body = document.body, html = document.documentElement; return  Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight ); })();");
+                if (task == null)
+                {
+                    return;
+                }
+
+                task.ContinueWith(t =>
+                    {
+                        if (!t.IsFaulted)
+                        {
+                            var response = t.Result;
+                            var EvaluateJavaScriptResult = response.Success ? (response.Result ?? "null") : response.Message;
+                            Console.WriteLine("Script result = {0}", EvaluateJavaScriptResult);
+                        }
+                    },
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            }, 
+            TimeSpan.FromSeconds(1));
+         
+        }
+
+        public static async Task Run(Action action, TimeSpan period, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(period, cancellationToken);
+                action();
+            }
+        }
+
+        public static Task Run(Action action, TimeSpan period)
+        {
+            return Run(action, period, CancellationToken.None);
         }
 
         private void MainWindow_Resize(object sender, EventArgs e)
