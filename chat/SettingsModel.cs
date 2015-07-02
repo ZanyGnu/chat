8226d1104dce03781ac0496cfe0dccdd2b6d9787
ZanyGnu <ajaymartin@gmail.com>
2015-07-01 22:41:12 -0700

    add a model for the settings

---
+++ chat/SettingsModel.cs
@@ -0,0 +1,25 @@
+﻿namespace chat
+{
+    using System.ComponentModel;
+    using System.Drawing.Design;
+    using System.Windows.Forms.Design;
+
+    public class SettingsModel
+    {
+        private static int SettingsVersion0 = 0;
+        private static int CurrentSettingsVersion = SettingsVersion0;
+        
+        [Browsable(false)]
+        public int SettingsVersion { get; set; }
+
+        [Browsable(false)]
+        public bool NotificationShown { get; set; }
+
+        public bool EscapeToMinimizeToTray { get; set; }
+
+        public bool MimimizeToTray { get; set; }
+
+        [EditorAttribute(typeof(FolderNameEditor), typeof(UITypeEditor))]
+        public string BrowserCacheLocation { get; set; }
+    }
+}
