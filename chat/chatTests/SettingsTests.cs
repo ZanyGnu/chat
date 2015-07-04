a697292fde11581ea386b34a0359e63491571e00
ZanyGnu <ajaymartin@gmail.com>
2015-07-04 12:37:11 -0700

    100% ut coverage for settings and settings model

--- chat/chatTests/SettingsTests.cs
+++ chat/chatTests/SettingsTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace chat.Tests
 {
@@ -19,6 +20,41 @@ namespace chat.Tests
             Assert.IsTrue(settings.CurrentSettings != null);
             // check default values
             Assert.IsTrue(settings.CurrentSettings.MimimizeToTray == true);
+        }
+
+        [TestMethod()]
+        public void NewSettingsCreationAndSaveTest()
+        {
+            string settingsFileName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Settings settings = new Settings(settingsFileName);
+            // check default values
+            Assert.IsTrue(settings.CurrentSettings.MimimizeToTray == true);
+
+            // change value, save and test that its persisted
+            settings.CurrentSettings.MimimizeToTray = false;
+            settings.Save();
+
+            Settings settingsRefreshed = new Settings(settingsFileName);
+            Assert.IsFalse(settingsRefreshed.CurrentSettings.MimimizeToTray);
+
+        }
+
+        [TestMethod()]
+        public void SettingsSaveTest()
+        {
+            // check when directory does not already exist
+            string settingsFileName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName(), Path.GetRandomFileName());
+            Settings settings = new Settings(settingsFileName);
+            // check default values
+            Assert.IsTrue(settings.CurrentSettings.MimimizeToTray == true);
+
+            // change value, save and test that its persisted
+            settings.CurrentSettings.MimimizeToTray = false;
+            settings.Save();
+
+            Settings settingsRefreshed = new Settings(settingsFileName);
+            Assert.IsFalse(settingsRefreshed.CurrentSettings.MimimizeToTray);
+
         }
     }
 }
