f5d2ffb4157b19810bdcca6903bcc713499e33b6
ZanyGnu <ajaymartin@gmail.com>
2015-07-05 22:02:42 -0700

    add main window basic test

---
+++ chat/chatTests/MainWindowTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chat.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void MainWindowTest()
+        {
+            MainWindow mainWindow = new MainWindow();
+            Assert.IsTrue(mainWindow != null);
+        }
+    }
+}
